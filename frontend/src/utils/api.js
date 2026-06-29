788859c841e73373b4c4e38c2b41bdec307c71ee
RAMU1320 <your-email@example.com>
2026-06-29 16:32:59 +0530

    update API URL to production backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://book-a-doctor-c07k.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
